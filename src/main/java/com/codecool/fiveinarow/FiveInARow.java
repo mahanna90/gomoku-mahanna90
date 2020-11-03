b8032ec6dbcb6b878395bfc1c727bef7d1eebb45
mahanna <mahanna@mahanna.com>
2020-11-03 15:42:36 +0100

    board size setting finished

--- src/main/java/com/codecool/fiveinarow/FiveInARow.java
+++ src/main/java/com/codecool/fiveinarow/FiveInARow.java
@@ -7,11 +7,47 @@ import java.util.Scanner;
 
 public class FiveInARow {
 
+    private static int numRows;
+    private static int numCols;
+
+    public static int getBoardHeight(){
+        while (true) {
+            Scanner rowsInput = new Scanner(System.in);
+            System.out.println("Enter number of rows (10-15): ");
+            try {
+                numRows = rowsInput.nextInt();
+                if (numRows >= 10 && numRows <= 15) {
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input!");
+            }
+        }
+        return numRows;
+    }
+
+    public static int getBoardWidth(){
+        while (true) {
+            Scanner rowsInput = new Scanner(System.in);
+            System.out.println("Enter number of columns (10-15): ");
+            try {
+                numCols = rowsInput.nextInt();
+                if (numCols >= 10 && numRows <= 15) {
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input!");
+            }
+        }
+        return numRows;
+    }
+
     public static void main(String[] args){
 
-        Game game = new Game(15, 15);
-        int[][] board = game.getBoard();
-//        System.out.println(Arrays.deepToString(board));
+        numRows = getBoardHeight();
+        numCols = getBoardWidth();
+
+        Game game = new Game(numRows, numCols);
         game.printBoard();
 
 //        game.enableAi(1);
